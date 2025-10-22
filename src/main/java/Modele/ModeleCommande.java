44ca65626697b79fde593035496f051ccd2aca45
Emilien Bodat <emilien.bodat1@etu.univ-lorraine.fr>
2025-10-22 09:23:52 +0200

    [ADD] Ajoute ModeleCommande.java

---
+++ src/main/java/Modele/ModeleCommande.java
@@ -0,0 +1,78 @@
+package Modele;
+
+import Fidelite.StrategyFidelite;
+import Pizza.Pizza;
+import javafx.beans.Observable;
+
+import java.util.ArrayList;
+
+public class ModeleCommande implements Sujet{
+    private int nbPizza;
+    private ArrayList<Pizza> listPizza;
+    private int numPizzaCourante;
+    private double prixCommande;
+    private ArrayList<Observateur> observateurs;
+    private StrategyFidelite taux;
+
+    public ModeleCommande() {
+        this.nbPizza = 0;
+        this.listPizza = new ArrayList<>();
+        this.numPizzaCourante = 0;
+        this.prixCommande = 0;
+        this.observateurs = new ArrayList<>();
+    }
+
+    public void ajouterPizza(String naturePizza){
+        if(this.nbPizza != 4) {
+            this.nbPizza++;
+            this.listPizza.add(new naturePizza());
+            this.numPizzaCourante++;
+            calculPrixCommande();
+        }
+    }
+
+    public void calculPrixCommande(){
+        this.prixCommande = 0;
+        for(Pizza p : this.listPizza){
+            this.prixCommande += p.cout();
+        }
+    }
+
+    public int getNbPizza() {
+        return nbPizza;
+    }
+
+    public double getPrixCommande() {
+        return prixCommande;
+    }
+
+    public ArrayList<Pizza> getListPizza() {
+        return listPizza;
+    }
+
+    public int getNumPizzaCourante() {
+        return numPizzaCourante;
+    }
+
+    public void setNumPizzaCourante(int numPizzaCourante) {
+        this.numPizzaCourante = numPizzaCourante;
+    }
+
+    public void setFidelite(StrategyFidelite fidelite){
+        this.taux = fidelite;
+    }
+
+    public void enregistrerObservateur(Observateur e){
+        this.observateurs.add(e);
+    }
+
+    public void supprimerObservateur(Observateur e){
+        this.observateurs.remove(e);
+    }
+
+    public void notifierObservateurs(){
+        for(Observateur e : this.observateurs){
+            e.actualiser(this);
+        }
+    }
+}
